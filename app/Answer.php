0b1cbcf763fc516f201e35e1b2f49341a58f5e60
Adnan Latic <adnanlatic@gmail.com>
2020-04-13 13:44:14 +0200

    Generate fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -7,14 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
   public function question(){
-    return $this->belongsTo('App\Answer');
+    return $this->belongsTo('App\Question');
   }
 
   public function user(){
-    return $this->belongsTo('App\Answer');
+    return $this->belongsTo('App\User');
   }
 
   public function getBodyHtmlAttribute(){
     return Parsedown::instance()->text($this->body);
+  }
+
+  public static function boot(){
+    parent::boot();
+
+    static::created(function($answer){
+      $answer->question->increment('answers_count');
+    });
   }
 }
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -10,7 +10,6 @@ $factory->define(Question::class, function (Faker $faker) {
         'title' => rtrim($faker->sentence(5, 10), "."),
         'body' => $faker->paragraphs(rand(3, 7), true),
         'views' => rand(0,1000),
-        'answers_count' => rand(0,10),
         'votes' => rand(-100,100),
     ];
 });
